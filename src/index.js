6b71cb50c5a5e0e85c27b0cdd1c58232bfae80ca
Danny Varner <dvarner@silverspringnet.com>
2016-08-20 17:19:03 -0700

    Basic Redux working showing 3 books

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-
 import App from './components/app';
-import reducers from './reducers';
+import {combineReducers, createStore} from 'redux';
+import BooksReducer from './reducers/reducer-books';
+import {Provider} from 'react-redux';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+const rootReducer = combineReducers({
+  books: BooksReducer
+});
+
+let store = createStore(rootReducer);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <App />
-  </Provider>
-  , document.querySelector('.container'));
+  </Provider>, document.querySelector('.container')
+);
---
+++ src/reducers/reducer-books.js
@@ -0,0 +1,7 @@
+export default function () {
+  return [
+    {title: "Book1", pages: 10},
+    {title: "Book2", pages: 3},
+    {title: "Book3", pages: 220}
+  ]
+}
